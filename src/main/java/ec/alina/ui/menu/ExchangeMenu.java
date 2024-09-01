9cff3978c60f5dedc209b1c893a2be50295b9337
Alina Carpio <agcarpio@espol.edu.ec>
2024-09-01 00:44:59 -0500

    FIX: Deposit money use case

--- src/main/java/ec/alina/ui/menu/ExchangeMenu.java
+++ src/main/java/ec/alina/ui/menu/ExchangeMenu.java
@@ -1,18 +1,20 @@
 package ec.alina.ui.menu;
 
 import ec.alina.domain.enums.CrytoType;
+import ec.alina.domain.enums.TransactionType;
+import ec.alina.domain.models.Transaction;
 import ec.alina.domain.models.User;
 import ec.alina.domain.models.Wallet;
-import ec.alina.domain.use_cases.DepositMoneyUseCase;
-import ec.alina.domain.use_cases.GetCurrentUseCase;
-import ec.alina.domain.use_cases.UserLogoutUseCase;
-import ec.alina.domain.use_cases.ViewWalletBalanceUseCase;
+import ec.alina.domain.use_cases.*;
+import ec.alina.domain.validations.InputValidator;
 import ec.alina.domain.validations.NumberValidator;
 import ec.alina.domain.validations.exceptions.IllegalAmountException;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.lang.System.out;
 
@@ -22,16 +24,17 @@ public class ExchangeMenu extends Menu {
     private final ViewWalletBalanceUseCase viewWalletBalanceUseCase;
     private final GetCurrentUseCase getCurrentUseCase;
     private final DepositMoneyUseCase depositMoneyUseCase;
+//    private final ViewTransactionHistoryUseCase viewTransactionHistoryUseCase;
     private final Scanner scanner;
     private final MenuNavigator navigator;
 
-    private final NumberValidator numberValidator = new NumberValidator();
 
     public ExchangeMenu(
             UserLogoutUseCase userLogoutUseCase,
             ViewWalletBalanceUseCase viewWalletBalanceUseCase,
             GetCurrentUseCase getCurrentUseCase,
             DepositMoneyUseCase depositMoneyUseCase,
+//            ViewTransactionHistoryUseCase viewTransactionHistoryUseCase,
             Scanner scanner,
             MenuNavigator navigator) {
         super("Crypto Exchange Menu");
@@ -39,14 +42,16 @@ public class ExchangeMenu extends Menu {
         this.viewWalletBalanceUseCase = viewWalletBalanceUseCase;
         this.getCurrentUseCase = getCurrentUseCase;
         this.depositMoneyUseCase = depositMoneyUseCase;
+//        this.viewTransactionHistoryUseCase = viewTransactionHistoryUseCase;
         this.scanner = scanner;
         this.navigator = navigator;
     }
 
     @Override
     public Menu build() {
-        addItem(new MenuItem(1, "View wallet balance", this::onViewWalletBalance));
-        addItem(new MenuItem(2, "Deposit money", this::onDeposit));
+        addItem(new MenuItem(1, "Deposit money", this::onDeposit));
+        addItem(new MenuItem(2, "View wallet balance", this::onViewWalletBalance));
+//        addItem(new MenuItem(3, "View transaction history", this::onViewTransactionHistory));
         addItem(new LogoutMenuItem(3, userLogoutUseCase, navigator));
         return this;
     }
@@ -70,22 +75,40 @@ public class ExchangeMenu extends Menu {
     private void onDeposit() {
         out.println("Enter the amount you want to deposit: ");
         String amount = scanner.nextLine();
-        BigDecimal depositAmount = new BigDecimal(amount);
 
-        if (!NumberValidator.isValidNumber(amount)) {
-            out.println("Invalid amount entered. Please enter a valid amount.");
+        if (!InputValidator.isValidNumber(amount)) {
+            out.println("Invalid amount entered. Please enter a valid number.");
+            return;
         }
 
+        BigDecimal depositAmount = new BigDecimal(amount);
+
         try {
-            numberValidator.validate(depositAmount, "Deposit amount");
             depositMoneyUseCase.invoke(getCurrentUseCase.invoke().getId(), depositAmount);
             out.println("Deposit successful");
-
         } catch (IllegalAmountException ex) {
             out.println("Something went wrong!");
             out.println(ex.getMessage());
-        }   catch (NumberFormatException ex) {
-            out.println("Invalid number format");
         }
     }
+
+
+//    private void onViewTransactionHistory() {
+//        AtomicInteger counter = new AtomicInteger(1);
+//        out.println("--------------- Transaction history ----------------");
+//        List<Transaction> userTransactions = viewTransactionHistoryUseCase.invoke(getCurrentUseCase.invoke().getId());
+//        userTransactions.forEach(transaction -> {
+//            String transactionNumber = String.format("TC%02d", counter.getAndIncrement());
+//            String amountSign = transaction.getTransactionType() == TransactionType.BUY ? "+" : "-";
+//            String formattedPrice = amountSign + "$" + transaction.getPrice().toString();
+//            String amount = transaction.getAmount().toString();
+//
+//            out.println("---------------- "+transactionNumber+ " ----------------");
+//            out.println("Transaction type: " + transaction.getTransactionType());
+//            out.println("Crypto currency: " + amount + transaction.getTransactionType());
+//            out.println("Price: " + formattedPrice);
+//            out.println();
+//        });
+//
+//    }
 }
